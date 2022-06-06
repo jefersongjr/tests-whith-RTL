ca0461dd3e4ff3f4254f55492bfcfbd9169d5f57
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-06 17:45:39 -0300

    Requisito 2, testando a imagem

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -18,4 +18,11 @@ describe('Teste se a página contém as informações sobre a Pokédex: ', () =>
     expect(paragraphsAbout1).toBeInTheDocument();
     expect(paragraphsAbout2).toBeInTheDocument();
   });
+
+  test(`Teste se a página contém a seguinte imagem de uma Pokédex: 
+       https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png.`, () => {
+    render(<About />);
+    const imageAbout = screen.getByAltText(/pokédex/i);
+    expect(imageAbout).toBeInTheDocument();
+  });
 });
