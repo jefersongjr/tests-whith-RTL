d9ce95770746bbdcac1d301b8b755791826afe09
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-06 17:41:38 -0300

    Requisito 2 , testando os paragráfos

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -2,11 +2,20 @@ import React from 'react';
 import { screen, render } from '@testing-library/react';
 import About from '../pages/About';
 
-describe('Teste se a página contém as informações sobre a Pokédex ', () => {
+describe('Teste se a página contém as informações sobre a Pokédex: ', () => {
   test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
     render(<About />);
     const aboutHeader = screen.getByText(/about pokédex/i);
 
     expect(aboutHeader).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex: ', () => {
+    render(<About />);
+    const paragraphsAbout1 = screen.getByText(/This application/i);
+    const paragraphsAbout2 = screen.getByText(/One can filter/i);
+
+    expect(paragraphsAbout1).toBeInTheDocument();
+    expect(paragraphsAbout2).toBeInTheDocument();
   });
 });
