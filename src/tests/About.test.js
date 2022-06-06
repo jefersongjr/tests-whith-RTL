a073e768e1b73d070b0dc71ef567391314d38f73
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-06 17:20:26 -0300

    requisito 2, testando render do h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../pages/About';
+
+describe('Teste se a página contém as informações sobre a Pokédex ', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const aboutHeader = screen.getByText(/about pokédex/i);
+
+    expect(aboutHeader).toBeInTheDocument();
+  });
+});
