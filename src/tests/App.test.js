d4a699dfe58487ae4f124c2b8f5673f6f6a0513d
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-06 17:01:08 -0300

    corrigindo lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import renderWithRouter from '../services/renderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 
 describe(`Teste se o topo da aplicação 
           contém um conjunto fixo de links de navegação: `, () => {
