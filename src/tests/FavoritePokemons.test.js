64ab81a2a5accb6b8bb34e8464e955f19c19cba2
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-07 16:44:02 -0300

    requisito 3, testando os pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
 
 describe('Testa a página de Pokemóns Favoritos', () => {
   test(`Teste se é exibida na tela a mensagem 
@@ -10,9 +11,22 @@ describe('Testa a página de Pokemóns Favoritos', () => {
 
     expect(notFavorites).toBeInTheDocument();
   });
-  test('Teste se são exibidos todos os cards de pokémons favoritados; ', () => {
-    render(<FavoritePokemons />);
-    const favoritePokemon = screen.findByTestId('pokemon-name');
-    expect(favoritePokemon).toBeInTheDocument();
+  test('Teste se são exibidos todos os cards de pokémons favoritados; ', async () => {
+    const favoritePokemons = [{
+      id: 25,
+      name: 'Pikachu',
+      type: 'Electric',
+      averageWeight: {
+        value: '6.0',
+        measurementUnit: 'kg',
+      },
+      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+    }];
+
+    renderWithRouter(<FavoritePokemons pokemons={ favoritePokemons } />);
+
+    const favorite = await screen.findByText(/pikachu/i);
+    expect(favorite).toBeInTheDocument();
   });
 });
