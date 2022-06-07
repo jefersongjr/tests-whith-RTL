0ebf6cad8290eb18d4fe0bd9ecf0acb637485baf
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-07 20:09:26 -0300

    requisito 5, testando botão nextPokemon

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,46 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import pokemons from '../data';
+import renderWithRouter from '../renderWithRouter';
+import Pokedex from '../pages/Pokedex';
+
+describe('Teste o componente <Pokedex />', () => {
+  const isPokemonFavoriteByIdType = {
+    10: false,
+    143: false,
+    148: false,
+    151: false,
+    23: false,
+    65: false,
+    78: false,
+    5: false,
+  };
+  beforeEach(() => renderWithRouter(<Pokedex
+    pokemons={ pokemons }
+    isPokemonFavoriteById={ isPokemonFavoriteByIdType }
+  />));
+  test(`Teste se a página contém um 
+      heading h2 com o texto Encountered pokémons; `, () => {
+    const headerPokedex = screen.getByText(/encountered pokémons/i);
+
+    expect(headerPokedex).toBeInTheDocument();
+  });
+
+  test(`Teste se é exibido o próximo pokémon da 
+  lista quando o botão Próximo pokémon é clicado: `, async () => {
+    const btnNextPokemon = screen.getByText(/próximo pokémon/i);
+
+    expect(btnNextPokemon).toBeInTheDocument();
+
+    const firstPokemon = screen.getByText(/pikachu/i);
+
+    expect(firstPokemon).toBeInTheDocument();
+
+    userEvent.click(btnNextPokemon);
+
+    const nextPokemon = screen.getByText(/Charmander/i);
+
+    expect(nextPokemon).toBeInTheDocument();
+  });
+});
